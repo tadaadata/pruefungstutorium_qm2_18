fb44df54bbf694e512f7da2511bddadf0c2b608c
jemus42 <lukas@quantenbrot.de>
2018-07-04 18:00:28 +0200

    AUTOMATIC DEPLOYMENT YO

--- build.R
+++ build.R
@@ -10,41 +10,13 @@ cat("Rendering single page version...\n\n")
 rmarkdown::render(input         = "presentation.Rmd",
                   output_format = "html_document",
                   output_file   = "index.html", quiet = TRUE)
-#
-# current_user <- Sys.info()[["user"]]
-#
-# cat("\nI hope you're really", current_user, "\n")
-#
-# if (current_user == "Lukas") {
-#   out_dir <- "~/Sync/public.tadaa-data.de/pruefungstutorien/qm_sose_18/"
-# } else if (current_user == "tobi") {
-#   out_dir <- "~/Dokumente/syncthing/public.tadaa-data.de/pruefungstutorien/qm_sose_18"
-# } else {
-#   out_dir <- ""
-# }
-#
-# if (out_dir == "") {
-#   cat("\n\n**No output directory defined, moving nothing**\n\n")
-# }
-#
-# out_docs   <- c(list.files(pattern = "*.html"), "presentation.Rmd")
-# out_assets <- c("assets", "img")
-#
-# copy_docs <- sapply(out_docs,   file.copy, to = out_dir, overwrite = T, recursive = F)
-# copy_assets <- sapply(out_assets, file.copy, to = out_dir, overwrite = T, recursive = T)
-#
-# if (all(copy_docs)) {
-#   cat("\nAll docs copied successfully\n")
-# }
-# if (all(copy_assets)) {
-#   cat("\nAll assets copied successfully\n")
-# }
+
 cat("\nAll done.\n")
 timestamp()
-
 
 library(slackr)
 slackr_setup(config_file = "/opt/tadaadata/.slackr")
 
-msg <- paste0(lubridate::now(tzone = "CEST"), ": Built Pruefungstutorium SoSe 18")
+msg <- paste0(lubridate::now(tzone = "CET"), ": Built Pruefungstutorium SoSe 18",
+              "Clicky: https://public.tadaa-data.de/pruefungstutorien/qm2_sose_18/presentation.html")
 text_slackr(msg, channel = "#tutorium", username = "tadaabot", preformatted = FALSE)
